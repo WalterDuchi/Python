5ed1fc5c040e60e660366e16b1ff059e079e63ad
Walter Alejandro Duchi Rivera <walter.duchiriv@ug.edu.ec>
2024-03-11 21:42:26 -0500

    11 de marzo 2

--- laboratorio.py
+++ laboratorio.py
@@ -1,18 +1,9 @@
-# Escribe un bucle for que cuente hasta cinco.
-# Cuerpo del bucle: imprime el número del contador y la palabra "Mississippi".
-# Cuerpo del bucle - usar: time.sleep (1)
+palabraSecreta = "chupacabra"
 
-# Importa el módulo time al principio de tu programa.
-import time
-
-for i in range(1, 6):
-    print(i, "Mississippi")
-    time.sleep(1)
-
-# Escribe un bucle while que cuente hasta cinco.
-i = 1
-while i <= 5:
-    print(i, "Mississippi")
-    # Cuerpo del bucle - usar: time.sleep (1)
-    time.sleep(1)
-    i += 1
+while True:
+    palabra = input("Introduce la palabra secreta: ")
+    if palabra == palabraSecreta:
+        print("¡Felicidades! Has adivinado la palabra secreta.")
+        break
+    else:
+        print("¡Inténtalo de nuevo!")
